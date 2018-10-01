525fddb86317739746ca5ff1aa373e7267def6e8
Ralf Escher <ralf.escher@gmx.de>
2018-10-01 22:45:57 +0200

    Ident Python code

--- raspberry.py
+++ raspberry.py
@@ -6,18 +6,17 @@ ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev
 ser.baudrate=9600
 def blink(pin):
 
+    GPIO.output(pin,GPIO.HIGH)  
+    time.sleep(1)  
+    GPIO.output(pin,GPIO.LOW)  
+    time.sleep(1)  
+    return
 
-GPIO.output(pin,GPIO.HIGH)  
-time.sleep(1)  
-GPIO.output(pin,GPIO.LOW)  
-time.sleep(1)  
-return
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)
+    while True:
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-while True:
-
-read_ser=ser.readline()
-print(read_ser)
-if(read_ser=="Hello From Arduino!"):
-blink(11)
+        read_ser=ser.readline()
+        print(read_ser)
+        if(read_ser=="Hello From Arduino!"):
+            blink(11)
